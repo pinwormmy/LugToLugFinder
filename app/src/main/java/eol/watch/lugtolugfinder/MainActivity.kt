d0a0f75bce48cc3395707460b26a4168634816ff
pinwormmy <ozkhan@naver.com>
2023-05-17 14:52:27 +0900

    머티리얼 디자인3 적용하는데 버전 오류 뜨는 중...
    버전 문제 왕창 뜨는거 다 잡아놨더니, 또 뜸.
    뭐 적용할 때마다 골치네 ㅋ
    
    - 파이어베이스 실시간DB 적용하기
    - 구글 로그인 도입

--- app/src/main/java/eol/watch/lugtolugfinder/MainActivity.kt
+++ app/src/main/java/eol/watch/lugtolugfinder/MainActivity.kt
@@ -1,13 +1,38 @@
 package eol.watch.lugtolugfinder
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContent {
+            Greeting("Android")
+        }
     }
 }
+
+@Composable
+fun Greeting(name: String) {
+    val colors = if (isSystemInDarkTheme()) {
+        Log.d("checkDarkMode","다크 모드 확인...")
+        darkColors(primary = Color.Black, onPrimary = Color.White)
+    } else {
+        lightColors(primary = Color.White, onPrimary = Color.Black)
+    }
+
+    MaterialTheme(colors = colors) {
+        Text(text = "Hello $name!", color = MaterialTheme.colors.onPrimary)
+    }
+}
+
