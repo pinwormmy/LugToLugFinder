e5f0af2af29902f948344b840c4446dec805df41
pinwormmy <ozkhan@naver.com>
2023-05-22 15:23:00 +0900

    하단 메뉴 구현 완료(다크테마 적용됨)
    시스템의 다크 테마 기준으로 하단메뉴 출력 완료
    텍스트없이 아이콘만 뜨게 함
    
    - 파이어베이스 실시간DB 적용하기
    - 구글 로그인 도입

--- app/src/main/java/eol/watch/lugtolugfinder/MainActivity.kt
+++ app/src/main/java/eol/watch/lugtolugfinder/MainActivity.kt
@@ -6,24 +6,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.darkColors
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Watch
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
 enum class BottomNavigationItem(val route: String, val icon: ImageVector, val title: String) {
     Search("search", Icons.Default.Search, "검색"),
@@ -31,14 +34,12 @@ enum class BottomNavigationItem(val route: String, val icon: ImageVector, val ti
     MyPage("mypage", Icons.Default.Person, "마이페이지")
 }
 
-
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Greeting("머티리얼 디자인3!")
+                // 여기에 원하는 컴포저블을 넣을 수 있습니다.
             }
         }
     }
@@ -70,89 +71,25 @@ fun MyApp(content: @Composable () -> Unit) {
     )
     var selectedItem by remember { mutableStateOf(BottomNavigationItem.Search) }
 
-    Scaffold(
-        bottomBar = {
-            BottomNavigation {
-                items.forEach { item ->
-                    BottomNavigationItem(
-                        icon = { Icon(item.icon, contentDescription = null) },
-                        label = { Text(item.title) },
-                        selected = selectedItem == item,
-                        onClick = { selectedItem = item }
-                    )
-                }
-            }
-        }
-    ) { paddingValues ->
-        Box(Modifier.padding(paddingValues)) {
-            //
-        }
-    }
-
     MaterialTheme(colors = colors) {
         Surface(color = MaterialTheme.colors.background) {
-            content()
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(text = "안뇽~ $name!") },
-                actions = {
-                    IconButton(onClick = { /* TODO: Handle click */ }) {
-                        Icon(Icons.Filled.Favorite, contentDescription = null)
+            Scaffold(
+                bottomBar = {
+                    BottomNavigation {
+                        items.forEach { item ->
+                            BottomNavigationItem(
+                                icon = { Icon(item.icon, contentDescription = null) },
+                                selected = selectedItem == item,
+                                onClick = { selectedItem = item }
+                            )
+                        }
                     }
                 }
-            )
-        },
-        floatingActionButton = {
-            ExtendedFloatingActionButton(
-                text = { Text("Extended FAB") },
-                onClick = { /* TODO: Handle click */ }
-            )
-        }
-    ) { innerPadding ->
-        BodyContent(Modifier.padding(innerPadding))
-    }
-}
-
-@Composable
-fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(text = "Hello World!")
-        TextButton(onClick = { /* TODO: Handle click */ }) {
-            Text("Text Button")
-        }
-        Button(onClick = { /* TODO: Handle click */ }) {
-            Text("Button")
-        }
-        Card(
-            modifier = Modifier.padding(top = 8.dp),
-            elevation = 8.dp
-        ) {
-            Column(modifier = Modifier.padding(16.dp)) {
-                Text(text = "카드형 공간입니다..")
-                TextField(
-                    value = "",
-                    onValueChange = { /* TODO: Handle text change */ },
-                    label = { Text("입력하시오") },
-                    modifier = Modifier.padding(top = 8.dp)
-                )
+            ) { paddingValues ->
+                Box(Modifier.padding(paddingValues)) {
+                    content()
+                }
             }
         }
     }
 }
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyApp {
-        Greeting("디폴트프리뷰")
-    }
-}
-
