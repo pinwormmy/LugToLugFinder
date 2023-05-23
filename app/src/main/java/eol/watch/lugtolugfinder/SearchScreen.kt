6b7cee768d4bccda83b86bdfd073b7b04f501818
pinwormmy <ozkhan@naver.com>
2023-05-23 15:07:28 +0900

    기본 페이지가 될 검색페이지 작업 중...
    그냥 머티리얼 디자인2 기반으로 작업하기로 하고 코드 되돌림.
    검색 페이지 코드는 구현했는데, 메인엑티비티 연동은 아직 안 한 상태.
    
    - 파이어베이스 실시간DB 적용하기
    - 구글 로그인 도입

--- app/src/main/java/eol/watch/lugtolugfinder/SearchScreen.kt
+++ app/src/main/java/eol/watch/lugtolugfinder/SearchScreen.kt
@@ -1,5 +1,3 @@
-@file:Suppress("OPT_IN_IS_NOT_ENABLED")
-
 package eol.watch.lugtolugfinder
 
 import androidx.compose.foundation.layout.Arrangement
@@ -9,16 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen() {
     Column(
@@ -30,8 +26,8 @@ fun SearchScreen() {
     ) {
         Text(
             text = "Lug to Lug Finder",
-            style = MaterialTheme.typography.bodyLarge,
-            color = MaterialTheme.colorScheme.onBackground
+            style = MaterialTheme.typography.body1,
+            color = MaterialTheme.colors.onSurface
         )
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
@@ -42,10 +38,3 @@ fun SearchScreen() {
         )
     }
 }
-
-
-
-
-
-
-
