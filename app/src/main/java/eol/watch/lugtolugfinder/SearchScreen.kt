2fc5f6933e83eaa68e8bf924cc1d831ee1531454
pinwormmy <ozkhan@naver.com>
2023-05-23 10:27:10 +0900

    기본 페이지가 될 검색페이지 작업 중...
    오류는 잡긴했는데, 검색 페이지는 머티리얼3,
    메인엑티비티는 머티리얼2로 되어있는 골때리는 상황.
    머티리얼3가 학습이 덜 되서, 문제가 되고 있는데,
    바로 배워서 쓰는거 해보고 안되면 머티리얼2로 개발 고려.
    검색 페이지 코드는 구현했는데, 메인엑티비티 연동은 아직 안 한 상태.
    
    - 파이어베이스 실시간DB 적용하기
    - 구글 로그인 도입

--- app/src/main/java/eol/watch/lugtolugfinder/SearchScreen.kt
+++ app/src/main/java/eol/watch/lugtolugfinder/SearchScreen.kt
@@ -1,3 +1,5 @@
+@file:Suppress("OPT_IN_IS_NOT_ENABLED")
+
 package eol.watch.lugtolugfinder
 
 import androidx.compose.foundation.layout.Arrangement
@@ -7,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -15,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen() {
     Column(
@@ -26,18 +30,19 @@ fun SearchScreen() {
     ) {
         Text(
             text = "Lug to Lug Finder",
-            style = MaterialTheme.typography.title, // updated to Material 3 style
-            color = MaterialTheme.colors.onPrimaryContainer // updated to Material 3 color
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onBackground
         )
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
-            value = text,
-            onValueChange = { text = it },
-            label = { Text("Label") }
+            value = "",
+            onValueChange = { /* TODO: Handle text change */ },
+            label = { Text("검색어를 입력하세요") },
+            modifier = Modifier.fillMaxWidth()
         )
-
     }
 }
+
 
 
 
