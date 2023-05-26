04fade8992c203539fd80560039f316a54591729
pinwormmy <ozkhan@naver.com>
2023-05-26 14:50:12 +0900

    파이어베이스 실시간db 적용한 검색결과 페이지 구현중
    근데, 검색 전/후 페이지 나누려했는데, 후페이지 형태로만 구성되버림
    그리고 검색버튼이 따로 없어서 진행 안되는 상태로 구현됨.
    수정 필요.(그래서 아직 db연동 잘 된건지 테스트도 못함)
    
    - 크기 데이터 반영한 시계 및 손목 실루엣 화면 구현
    - 구글 로그인 도입
    - 회원관리 및 관리자 모드를 통한 데이터관리 승인 및 신고 처리

--- app/src/main/java/eol/watch/lugtolugfinder/SearchScreen.kt
+++ app/src/main/java/eol/watch/lugtolugfinder/SearchScreen.kt
@@ -7,34 +7,74 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.tasks.Task
+import com.google.firebase.database.FirebaseDatabase
+import eol.watch.lugtolugfinder.model.Watch
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 @Composable
 fun SearchScreen() {
+    var searchText by remember { mutableStateOf("") }
+    val searchResults = remember { mutableStateOf(listOf<Watch>()) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Lug to Lug Finder",
-            style = MaterialTheme.typography.body1,
-            color = MaterialTheme.colors.onSurface
-        )
+        // Display search results
+        searchResults.value.forEach { watch ->
+            // TODO: Display each watch
+        }
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
-            value = "",
-            onValueChange = { /* TODO: Handle text change */ },
+            value = searchText,
+            onValueChange = { newValue ->
+                searchText = newValue
+            },
             label = { Text("검색어를 입력하세요") },
             modifier = Modifier.fillMaxWidth()
         )
+        // Trigger search when searchText changes
+        LaunchedEffect(searchText) {
+            searchResults.value = searchWatchesInDatabase(searchText)
+        }
+    }
+}
+
+suspend fun searchWatchesInDatabase(searchText: String): List<Watch> = withContext(Dispatchers.IO) {
+    val database = FirebaseDatabase.getInstance()
+    val myRef = database.getReference("watches")
+
+    val snapshot = myRef.get().await()
+    return@withContext snapshot.children.mapNotNull { it.getValue(Watch::class.java) }
+        .filter { it.brand.contains(searchText, ignoreCase = true) ||
+                it.name.contains(searchText, ignoreCase = true) ||
+                it.productNumber.contains(searchText, ignoreCase = true) }
+}
+
+suspend fun <T> Task<T>.await(): T = suspendCoroutine { continuation ->
+    addOnSuccessListener { result ->
+        continuation.resume(result)
+    }
+    addOnFailureListener { exception ->
+        continuation.resumeWithException(exception)
     }
 }
---
+++ app/src/main/java/eol/watch/lugtolugfinder/model/Watch.kt
@@ -0,0 +1,11 @@
+package eol.watch.lugtolugfinder.model
+
+data class Watch(
+    val brand: String,
+    val name: String,
+    val productNumber: String,
+    val caseLength: Double,
+    val lugToLugLength: Double,
+    val writer: String
+)
+
