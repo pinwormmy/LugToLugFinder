f80b299916da731e2f0fa7eccbc6ff8cb4af0f40
pinwormmy <ozkhan@naver.com>
2024-11-28 01:08:38 +0900

    태이블 태그 간단하게 만듬

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          그냥 한글 써봄
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <p>개발환경 변경...ㄱㄱ</p>
+      <input placeholder='brand'/>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Brand</th>
+            <th>Collection</th>
+            <th>Ref</th>
+            <th>Case(mm)</th>
+            <th>Lug to Lug(mm)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Omega</th>
+            <th>Constellation Globemaster</th>
+            <th>130.30.39.21.03.001</th>
+            <th>39</th>
+            <th>46.7</th>
+          </tr>
+          <tr>
+            <th>Omega</th>
+            <th>Sea Master Aqua Terra</th>
+            <th>220.10.41.21.01.002</th>
+            <th>41</th>
+            <th>48</th>
+          </tr>
+          <tr>
+            <th>Rolex</th>
+            <th>Submariner</th>
+            <th>126610LN</th>
+            <th>41</th>
+            <th>47.6</th>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
